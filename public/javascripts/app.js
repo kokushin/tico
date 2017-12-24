70bf4b7e1c1691e39df6e37333e4c9deeaa1ac85
kokushin <mail@kokush.in>
2017-12-24 21:51:12 +0900

    download system: Incomplete

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,18 +3,61 @@
   var app = new Vue({
     el: '#app',
     data: {
-      text: 'x',
-      type: 'round',
-      textColor: 'black',
-      backgroundColor: 'white'
+      text: 't',
+      type: 'square',
+      textColor: 'white',
+      backgroundColor: 'black',
+      size: [
+        16,
+        32,
+        96,
+        114,
+        144,
+        180,
+        500
+      ]
     },
     methods: {
       getImageData () {
         var _this = this
-        html2canvas(document.querySelector("#preview"), {
-          backgroundColor: null
-        }).then(function (canvas) {
-          _this.sendImageData(canvas.toDataURL())
+        var pushCount = 0
+        var data = []
+        var imageSizeLength = _this.size.length
+        var imageSize = 0
+        var defaltImageSize = document.getElementById('preview').clientWidth
+
+        new Promise(function (resolve, reject) {
+          for (var i = 0; i < imageSizeLength; i++) {
+            imageSize = _this.size[i]
+
+            html2canvas(document.querySelector("#preview"), {
+              backgroundColor: null,
+              logging: false,
+              scale: imageSize / defaltImageSize,
+              width: imageSize / (imageSize / defaltImageSize),
+              height: imageSize / (imageSize / defaltImageSize),
+            })
+              .then(function (canvas) {
+                if (imageSizeLength === 1) {
+                  data.push({
+                    size: _this.size[pushCount],
+                    src: canvas.toDataURL()
+                  })
+                  resolve()
+                } else {
+                  data.push({
+                    size: _this.size[pushCount],
+                    src: canvas.toDataURL()
+                  })
+                  pushCount++
+                  if (pushCount === imageSizeLength) {
+                    resolve()
+                  }
+                }
+              })
+          }
+        }).then(function () {
+          _this.sendImageData(data)
         })
       },
       sendImageData (data) {
--- routes/download.js
+++ routes/download.js
@@ -7,24 +7,34 @@ const router = express.Router()
 let uuid = ''
 
 router.post('/', (req, res) => {
-  const data = req.body.data.split(',')[1]
-  const src = base64.decode(data)
+  const data = req.body.data
+  let src = []
 
-  createImageFile(src)
+  for (let i = 0; i < data.length; i++) {
+    src.push({
+      size: data[i].size,
+      image: base64.decode(data[i].src.split(',')[1])
+    })
+  }
+
+  createImageFile(src, res)
 })
 
-const createImageFile = (src) => {
+const createImageFile = (src, res) => {
   uuid = uuidv4()
 
   fs.mkdir(`cache/${uuid}`, (err) => {
     if (!err) {
-      fs.writeFile(`cache/${uuid}/icon.png`, src, (_err) => {
-        if (!_err) {
-          console.log(`Created cache/${uuid}/icon.png`)
-        } else {
-          throw _err
-        }
-      })
+      for (let i = 0; i < src.length; i++) {
+        fs.writeFile(`cache/${uuid}/icon${src[i].size}x${src[i].size}.png`, src[i].image, (_err) => {
+          if (!_err) {
+            console.log(`Created cache/${uuid}/icon${src[i].size}x${src[i].size}.png`)
+          } else {
+            throw _err
+          }
+        })
+      }
+      res.end()
     } else {
       throw err
     }
